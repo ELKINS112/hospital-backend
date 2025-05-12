063b85d2c8bfee92f5faf334013e3df08b9795e3
ELKINS112 <58363457+ELKINS112@users.noreply.github.com>
2025-05-12 17:33:38 +0100

    Add files via upload

--- routes/admin.py
+++ routes/admin.py
@@ -1,1 +1,10 @@
-# admin routes
+from flask import Blueprint, jsonify
+from auth import token_required, role_required
+
+admin_bp = Blueprint('admin_bp', __name__, url_prefix='/admin')
+
+@admin_bp.route('/dashboard')
+@token_required
+@role_required('admin')
+def admin_dashboard(current_user):
+    return jsonify({'message': f'Welcome Admin {current_user.email}'})
