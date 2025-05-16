ac479dd61e2d8e661440be4e62c27400efd47d9b
ELKINS112 <58363457+ELKINS112@users.noreply.github.com>
2025-05-16 15:14:13 +0100

    Add files via upload

---
+++ create_all_role_users.py
@@ -0,0 +1,25 @@
+from extensions import db
+from models.user import User
+from werkzeug.security import generate_password_hash
+
+def create_users():
+    users = [
+        {"email": "admin@example.com", "password": "admin123", "role": "admin"},
+        {"email": "doctor@example.com", "password": "doctor123", "role": "doctor"},
+        {"email": "nurse@example.com", "password": "nurse123", "role": "nurse"},
+        {"email": "lab@example.com", "password": "lab123", "role": "lab"},
+        {"email": "pharmacist@example.com", "password": "pharma123", "role": "pharmacist"},
+        {"email": "accountant@example.com", "password": "account123", "role": "accountant"},
+        {"email": "patient@example.com", "password": "patient123", "role": "patient"},
+    ]
+
+    for u in users:
+        if not User.query.filter_by(email=u["email"]).first():
+            user = User(
+                email=u["email"],
+                password_hash=generate_password_hash(u["password"]),
+                role=u["role"]
+            )
+            db.session.add(user)
+
+    db.session.commit()
