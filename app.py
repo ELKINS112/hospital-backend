c6ad3d849e21c5cf720f4ff52df8072795c59b2b
ELKINS112 <58363457+ELKINS112@users.noreply.github.com>
2025-05-17 00:58:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from create_all_role_users import create_users
 
 from flask_cors import CORS
 app = Flask(__name__)
-#CORS(app, origins=["https://hospital-frontend-fokzrjik5-elkins-projects-6867ff50.vercel.app"])
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
 
