9327b3349983cb851132252583970771223f6951
ELKINS112 <58363457+ELKINS112@users.noreply.github.com>
2025-05-17 08:05:17 +0100

    Add files via upload

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,1 +1,28 @@
+from functools import wraps
+from flask import request, jsonify
+import jwt
+from models.user import User
 
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 403
+        try:
+            data = jwt.decode(token, 'secret_key', algorithms=['HS256'])
+            current_user = User.query.get(data['user_id'])
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 403
+        return f(current_user, *args, **kwargs)
+    return decorated
+
+def role_required(role):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(current_user, *args, **kwargs):
+            if current_user.role != role:
+                return jsonify({'message': 'Access forbidden'}), 403
+            return f(current_user, *args, **kwargs)
+        return wrapper
+    return decorator
